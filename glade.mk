6d469e72dbece29bc2ad4427f263fce1b9a4dbdd
rohan purohit <purohit.rohan@gmail.com>
2015-04-20 08:31:54 +0200

    add missing changes

--- glade.mk
+++ glade.mk
@@ -17,10 +17,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/glade/configs/common.mk)
 
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
@@ -28,5 +25,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6603/D6603:4.4.4/23.0.A.2.93/ovf_Rw:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6603-user 4.4.4 23.0.A.2.93 ovf_Rw release-keys"
 
-PRODUCT_NAME := validus_z3
+PRODUCT_NAME := glade_z3
 PRODUCT_DEVICE := z3
